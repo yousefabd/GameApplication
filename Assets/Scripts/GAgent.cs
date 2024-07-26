a06ad972961dbd3faaa8df3516e390053dd2de52
NizarMr-11 <121380447+NizarMr-11@users.noreply.github.com>
2024-07-26 16:42:54 +0300

    Planner class Modifications

--- Assets/Scripts/GAgent.cs
+++ Assets/Scripts/GAgent.cs
@@ -12,10 +12,12 @@ public class SubGoal
     //This bool is only used when THIS goal is required to happen once "SPAWNING"
     //So after this goal is satisfied we don't actually need to keep it in the list of
     //the actions for this agent
+    
     public bool remove;
 
     public SubGoal(string s, int i, bool r) {
     sgoals = new Dictionary<string,int>();
+    //An i integer is for the IMPORTANCE of the goal
     sgoals.Add(s, i);
     remove = r;
     }
@@ -25,7 +27,7 @@ public class GAgent : MonoBehaviour
     public List <GAction>  actions = new List<GAction>();
     public Dictionary<SubGoal,int> goals = new Dictionary<SubGoal,int>();
 
-    //GPlanner planner;
+    GPlanner planner;
     Queue<GAction> actionQueue;
     public GAction currentAction;
     SubGoal currentGoal;
@@ -43,6 +45,27 @@ public class GAgent : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        //if (currentAction != null && currentAction.running)
+        //{
+           //if(currentAction.agent.hasPath && currentAction.agent.remainingDistance)
         
+       // }
+        if (planner == null || actionQueue == null)
+        //  Agent has no plan
+        {
+            planner = new GPlanner();
+            //Sort through the agents till the most important goal is found
+            var sortedGoals = from entry in goals orderby entry.Value descending select entry;
+            foreach (KeyValuePair<SubGoal, int> sg in sortedGoals)
+            {
+                actionQueue = planner.Plan(actions , sg.Key.sgoals,/*belifs of the agent*/null);
+                if (actionQueue != null) 
+                {
+                currentGoal = sg.Key;
+                break;
+                }
+            }
+        }
+
     }
 }
--- Assets/Scripts/GPlanner.cs
+++ Assets/Scripts/GPlanner.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//For soring Things like dictionary
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 public class Node 
 {
@@ -15,6 +17,7 @@ public class Node
     {
         this.parent = parent;
         this.cost = cost;
+        //To make sure we get a copy of all states not the actual all states
         this.state = new Dictionary<string, int>(allStates)  ;
         this.action = action;
     }
@@ -36,34 +39,37 @@ public class GPlanner
         List<Node> leaves = new List<Node>();
         Node start = new Node(null, 0, GWorld.Instance.GetWorld().GetStates(), null);
 
-       // bool success = BuildGraph(start, leaves, usableActions, goal);
+        bool success = BuildGraph(start, leaves, usableActions, goal);
         
-      //  if (!success) 
-      //  {
-         //   Debug.Log("NO PLAN");
-           // return null;
-       // }
+        if (!success) 
+        {
+            Debug.Log("NO PLAN");
+            return null;
+        }
 
         Node cheapest = null;
+
+        //Evalyating the cheapest node
         foreach (Node leaf in leaves)
         {
             if (cheapest == null)
                 cheapest = leaf;
-            
+          
             else 
             {
                 if (leaf.cost < cheapest.cost)
                     cheapest = leaf;
             }
-        
         }
+
+        //Forming the sequence of achievable and cheapest actions backwardly
         List<GAction> result = new List<GAction>();
         Node n = cheapest;
         while (n != null)
         {
             if (n.action != null)
             {
-                result.Insert(0, n.action);
+                result.Insert(0,n.action);
             }
             n = n.parent;
         }
@@ -79,5 +85,59 @@ public class GPlanner
         }
 
         return queue;
+    }
+    private bool BuildGraph(Node parent, List<Node> leaves, List<GAction> usableActions, Dictionary<string, int> goal)
+    {
+        bool foundPath = false;
+        foreach (GAction action in usableActions)
+        {
+            if (action.IsAchievableGiven(parent.state))
+            { 
+            Dictionary<string,int>currentState = new Dictionary<string,int>(parent.state );
+
+                foreach (KeyValuePair<string, int> eff in action.effects) 
+                {
+                    if(!currentState.ContainsKey(eff.Key))
+                        currentState.Add(eff.Key, eff.Value);
+                
+                }
+
+                Node node = new Node(parent, parent.cost + action.cost, currentState, action);
+                if (GoalAchieved(goal, currentState))
+                {
+                    leaves.Add(node);
+                    foundPath = true;
+                }
+                else 
+                {
+                    List<GAction> subset = ActionSubset(usableActions, action);
+                    bool found = BuildGraph(node, leaves, subset, goal);
+                    if (found)
+                    { foundPath = true; } }
+            }
+        }
+        return foundPath;
+    }
+    private bool GoalAchieved(Dictionary<string, int> goal, Dictionary<string, int> state)
+    {
+        foreach (KeyValuePair<string, int> g in goal)
+        {
+            if (!state.ContainsKey(g.Key)) 
+            {
+                return false;
+            }        
+
+        }
+        return true;
+    }
+    private List<GAction> ActionSubset(List<GAction> actions, GAction removeMe)
+    {
+        List<GAction> subset = new List<GAction>();
+        foreach (GAction a in actions) 
+        {
+            if(!a.Equals(removeMe))
+                subset.Add(a);
+        }
+        return subset;
     }
 }
